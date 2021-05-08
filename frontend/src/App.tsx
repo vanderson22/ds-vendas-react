391ab2d68d432cb6d689d79c9adbf925ab2337c0
vanderson <vanderson.01@hotmail.com>
2021-05-08 20:15:24 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
-//import React from 'react';
- 
+import React from 'react';
+import NavBar from "components/navbar";
+import Footer from 'components/footer';
+import DataTable from 'components/dataTable';
 
+//retorno só pode ter um elemento, para isso usar o fragment <> </>
 function App() {
   return (
-    <div>
-          <h1>Olá mundo!</h1>
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+      
+        <DataTable />  
+      </div>
+     
+      <Footer />
+    </React.Fragment>
   );
 }
 
---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,16 @@
+//sintaxe lambda
+
+import React from 'react';
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://github.com/vanderson22" target="_blank" rel="noreferrer">Vanderson Nogueira</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
