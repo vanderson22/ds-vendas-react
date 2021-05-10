a562b510b3809070ca4346b04ce78bf5046771ab
vanderson <vanderson.01@hotmail.com>
2021-05-09 23:10:12 -0300

    url change

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,4 +1,4 @@
-export const BASE_URL= process.env.REACT_APP_BACKEND_URL ?? 'https://sds3-nogueira.herokuapp.com'
+export const BASE_URL='https://sds3-nogueira.herokuapp.com'
 
 export const AMOUNT = '/sales/amount-by-seller'
 export const SUCCESS = '/sales/success-by-seller'
